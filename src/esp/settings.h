ce42c7487c56f6cabf3c7edcff389cf476dc2dbf
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2025-02-09 17:37:25 +0300

    Добавил константы пинов

--- src/esp/settings.h
+++ src/esp/settings.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Arduino.h>
+
 
 #if __has_include("secrets.h")
 #include "secrets.h"
@@ -13,3 +15,17 @@ static constexpr auto CHAT_ID = "0";
 
 #define BOT_ACTIVE
 static constexpr auto TBOT_NAME = "@shs_SmartBank_bot";
+
+static constexpr auto p_A = D4;
+static constexpr auto p_B = D5;
+static constexpr auto p_C = D6;
+static constexpr auto p_D = D7;
+static constexpr auto p_E = D8;
+static constexpr auto p_F = 3;
+static constexpr auto p_G = 1;
+
+
+static constexpr auto p_DIGIT_1 = D0;
+static constexpr auto p_DIGIT_2 = D1;
+static constexpr auto p_DIGIT_3 = D2;
+static constexpr auto p_DIGIT_4 = D3;
